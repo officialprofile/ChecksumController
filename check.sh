fd7d576b24a1dd6c512aba6a0e23c70faf625010
officialprofile <75314157+officialprofile@users.noreply.github.com>
2023-03-23 11:49:29 +0100

    🧡

--- check.sh
+++ check.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 RED='\033[0;31m'
-GREEN='\033[0;31m'
+GREEN='\033[0;32m'
 NORMAL='\033[0m'
-
-cat test.txt | while read line 
+IFS=' '
+#echo "Enter the file name: "
+#read VAR
+while read line
 do
-   printf "${RED} $line\n"
-done
+   read -a data <<< "$line"
+   md5=$(md5sum "${data[1]}")
+   #echo ${data[0]} | cut -d ' ' -f 1 
+   if [[ "$line" == "$md5" ]]; then
+      printf "${GREEN}ZGODNOŚĆ${NORMAL} \t${data[1]}\n"
+   else
+      printf "${RED}\U02620 BŁĄD \U02620${NORMAL} \t${data[1]}\n"
+   fi
+#done < "$VAR"
+done < example_files/verify.txt
